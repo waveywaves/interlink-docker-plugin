d214cadac624ba267cf2188d0a44f9fd774c429b
Diego Ciangottini <diego.ciangottini@pg.infn.it>
2024-05-31 15:03:00 +0200

    revert ulimits

--- pkg/docker/Create.go
+++ pkg/docker/Create.go
@@ -174,7 +174,6 @@ func (h *SidecarHandler) CreateHandler(w http.ResponseWriter, r *http.Request) {
 
 				if container.Resources.Limits.Memory().Value() != 0 {
 					memoryLimitsArray = append(memoryLimitsArray, "--memory", strconv.Itoa(int(container.Resources.Limits.Memory().Value()))+"b")
-					uLimitMemoryArray = append(uLimitMemoryArray, "--ulimits", "\"memlock="+strconv.Itoa(int(container.Resources.Limits.Memory().Value()))+"\"")
 				}
 				if container.Resources.Limits.Cpu().Value() != 0 {
 					cpuLimitsArray = append(cpuLimitsArray, "--cpus", strconv.FormatFloat(float64(container.Resources.Limits.Cpu().Value()), 'f', -1, 64))
